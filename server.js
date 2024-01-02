fb532adba0328d78fe9294067dae611c9968e913
Kartik Kocher <kocher.kartik@gmail.com>
2024-01-02 19:07:54 +0530

    added required files for beanstalk

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+// Proxy setup for API requests
+app.use('/api', createProxyMiddleware({ 
+  target: 'http://wms-backend-dev4.us-west-1.elasticbeanstalk.com', 
+  changeOrigin: true,
+}));
+
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
