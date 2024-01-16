b941ad77469c0ed7ac3ba8c54de014e3272a54d1
kartik143 <38079397+kartik143@users.noreply.github.com>
2024-01-16 22:27:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
